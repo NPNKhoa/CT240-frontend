a8ccb49c4f463b655133a89351d38fc888e046e8
MPhus <phu3009@gmail.com>
2024-10-23 11:59:20 +0700

    Setting for Dang code

---
+++ src/pages/Home/Overview.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+function Overview() {
+	return (
+		<Box sx={{ maxWidth: '1200px', minWidth: '1200px', m: '20px auto', }}>
+			Đăng làm ở đây
+
+		</Box>
+	)
+}
+
+export default Overview
